e5ced715cdb5d9b5edde2e298f3e189dae724954
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2018-02-27 07:28:22 +0000

    Allow write to handle hex input

--- src/app/main.c
+++ src/app/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stddef.h>
+#include <string.h>
 
 #include <mem/mem.h>
 
@@ -10,8 +12,13 @@ int main(int argc, char ** argv)
     printf("mem utility testing version\n");
 
     if (argc < 2) {
-        printf("Please provide an address: mem <address>\n");
+        printf("Please provide an address: mem <address> [value to write]\n");
         return -1;
+    }
+
+    int write = 0;
+    if (argc >= 3) {
+        write = 1;
     }
 
     void * read_addr = NULL;
@@ -21,14 +28,13 @@ int main(int argc, char ** argv)
     if (read_addr == NULL)
         return -1;
 
-
     mem_context mem;
     #ifndef MMAP
     printf("non-mmap version\n");
-    if (mem_ctor(&mem, MEM_FILE, 0, NULL, NULL) != MEM_OKAY) {
+    if (mem_ctor(&mem, MEM_FILE, write, NULL, NULL) != MEM_OKAY) {
     #else
     printf("mmap version\n");
-    if (mem_ctor(&mem, MEM_MMAP, 0, (void *) read_addr,
+    if (mem_ctor(&mem, MEM_MMAP, write, (void *) read_addr,
                 (void *) ((uintptr_t) read_addr + sizeof(uint32_t) - 1))
         != MEM_OKAY) {
     #endif
@@ -36,15 +42,32 @@ int main(int argc, char ** argv)
         return -1;
     }
 
-    uint32_t val = 0xDEADBEEF;
-    int bytes_read = mem_read(&mem, read_addr, &val, sizeof(val));
-    if (bytes_read < 0) {
-        printf("Read failed (returned -1)\n");
+    int32_t val = 0xDEADBEEF;
+    int bytes_xfer = -1;
+    if (write) {
+        // write
+        size_t len = strlen(argv[2]);
+        if (len > 2 && argv[2][0] == '0' && argv[2][1] == 'x') {
+            sscanf(argv[2], "0x%x", &val);
+        } else {
+            sscanf(argv[2], "%d", &val);
+        }
+        bytes_xfer = mem_write(&mem, read_addr, &val, sizeof(val));
+        if (bytes_xfer < 0) {
+            printf("Write failed (returned -1)\n");
+        } else {
+            printf("[%p] = 0x%08x\n", read_addr, val);
+        }
     } else {
-        printf("[%p] = 0x%08x\n", read_addr, val);
+        bytes_xfer = mem_read(&mem, read_addr, &val, sizeof(val));
+        if (bytes_xfer < 0) {
+            printf("Read failed (returned -1)\n");
+        } else {
+            printf("[%p] = 0x%08x\n", read_addr, val);
+        }
     }
 
     mem_dtor(&mem);
 
-    return bytes_read >= 0 ? 0 : -1;
+    return bytes_xfer >= 0 ? 0 : -1;
 }
